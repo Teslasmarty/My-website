aa441316c8b6f21961f1ad5d5bad1402204f96a6
Teslasmarts <aruiz.sh@mysummitps.org>
2015-05-01 01:22:12 +0000

    update

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -2,7 +2,7 @@ function displayTime() {
     var currentTime = new Date();
     var hours = currentTime.getHours();
     var meridiem = "PM";
-    if (hours > 12) {
+    if (hours < 12) {
     hours = hours - 12;
     meridiem = "AM";
 }
